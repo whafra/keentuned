9c6a21980dbc9dbdf266541a1a93664c4cc7b068
liuwengchao <yuxiongkong159@gmail.com>
2022-01-27 16:41:36 +0800

    fix: rollback ack_tpce_test_db.py ack_tpch_test_db.py
    Signed-off-by: liuwengchao <yuxiongkong159@gmail.com>

--- daemon/examples/benchmark/tpch/ack_tpch_test_db.py
+++ daemon/examples/benchmark/tpch/ack_tpch_test_db.py
@@ -5,15 +5,13 @@ import sys
 import subprocess
 import logging
 logger = logging.getLogger(__name__)
-# const
-DEFAULT_CONNECTIONS = 300
-DEFAULT_THREADS = 10
-DEFAULT_DURATION = 2
+
+
 class Benchmark():
     def __transfMeasurement(self,value,measurement):
         if measurement == '':
             return value
-        
+
         # measurement of TPCH_time/Total_time
         elif measurement == 'h':
             return value * 60 * 60 * 10 ** 6
@@ -25,20 +23,20 @@ class Benchmark():
             return value * 10 ** 3
         elif measurement == 'us':
             return value
-        
+
         else:
             logger.warning("Unknown measurement: {}".format(measurement))
             return value
-        
+
     def run(self):
         """Run benchmark and parse output
-        
+
         Return True and score list if running benchmark successfully, otherwise return False and empty list.
         """
-        cmd = '/root/tpch/tpch.sh'
+        cmd = 'cd /root/tpch/; sh tpch.sh'
         logger.info(cmd)
         result = subprocess.run(
-                    cmd, 
+                    cmd,
                     shell=True,
                     close_fds=True,
                     stderr=subprocess.PIPE,
@@ -48,28 +46,24 @@ class Benchmark():
         self.error = result.stderr.decode('UTF-8','strict')
         if result.returncode == 0:
             logger.info(self.out)
-            
+
             pattern_TPCH_time = re.compile(r'TPCH test time:\s+([\d.]+)(\w+)')
             pattern_Total_time = re.compile(r'total time:\s+([\d.]+)(\w+)')
-            
+
             if not re.search(pattern_TPCH_time,self.out) \
                 or not re.search(pattern_Total_time,self.out):
                 logger.error("can not parse output: {}".format(self.out))
                 return False, []
-            
+
             _TPCH_time = float(re.search(pattern_TPCH_time,self.out).group(1))
-            TPCH_time_measurement = re.search(pattern_TPCH_time,self.out).group(2)
-            TPCH_time = self.__transfMeasurement(_TPCH_time, TPCH_time_measurement)
-            
+
             _Total_time = float(re.search(pattern_Total_time,self.out).group(1))
-            Total_time_measurement = re.search(pattern_Total_time,self.out).group(2)
-            Total_time = self.__transfMeasurement(_Total_time, Total_time_measurement)
-            
+
             result = {
-                "TPCH_Time": TPCH_time,
-                "Total_Time": Total_time,
+                "TPCH_Time": _TPCH_time,
+                "Total_Time": _Total_time,
             }
-            
+
             result_str = ", ".join(["{} = {}".format(k,v) for k,v in result.items()])
             print(result_str)
             return True, result_str
