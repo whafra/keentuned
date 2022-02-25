d43ce8c5e0f0d58142e222e4fc01a04f0b21b44a
liuwengchao <yuxiongkong159@gmail.com>
2022-02-25 18:45:23 +0800

    feat: keentuned support iperf macro detect
    Signed-off-by: liuwengchao <yuxiongkong159@gmail.com>

--- daemon/modules/parameter.go
+++ daemon/modules/parameter.go
@@ -1,9 +1,16 @@
 package modules
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"keentune/daemon/common/config"
 	"keentune/daemon/common/log"
 	"keentune/daemon/common/utils"
+	"keentune/daemon/common/utils/http"
+	"reflect"
+	"regexp"
+	"strings"
 )
 
 // Parameter define a os parameter value scope, operating command and value
@@ -24,6 +31,14 @@ type Parameter struct {
 	Success    bool          `json:"suc,omitempty"`
 	Base       interface{}   `json:"base,omitempty"`
 }
+
+type DetectResult struct {
+	Success bool        `json:"suc"`
+	Value   int         `json:"value"`
+	Message interface{} `json:"msg"`
+}
+
+const defMarcoString = "#!([0-9A-Za-z_]+)#"
 
 // updateParameter update the partial param by the total param
 func updateParameter(partial, total *Parameter) {
@@ -135,6 +150,17 @@ func assembleParam(param map[string]interface{}, domainName, paramName string) (
 	//  delete `desc` field, not used in any request body
 	delete(param, "desc")
 
+	if paramName == "Parallel" {
+		param["name"] = paramName
+		param["domain"] = domainName
+		if err := detectParam(param, paramName); err != nil {
+			return Parameter{}, fmt.Errorf("detect macro defination param:%v", err)
+		}
+
+		delete(param, "name")
+		delete(param, "domain")
+	}
+
 	var initParam Parameter
 	initParamMap := make(map[string]interface{})
 	for name, value := range param {
@@ -150,3 +176,107 @@ func assembleParam(param map[string]interface{}, domainName, paramName string) (
 	initParam.ParaName = paramName
 	return initParam, nil
 }
+
+func detectParam(param map[string]interface{}, paramName string) error {
+	if paramName != "Parallel" {
+		return nil
+	}
+	ranges, ok := param["range"].([]interface{})
+	if !ok {
+		return fmt.Errorf("assert range to slice interface failed, real type %v", reflect.TypeOf(param["range"]))
+	}
+	var range2Int []int
+	var detectedMacroValue = make(map[string]int)
+	for _, v := range ranges {
+		value, ok := v.(float64)
+		if ok {
+			range2Int = append(range2Int, int(value))
+			continue
+		}
+		macroString, ok := v.(string)
+		re, _ := regexp.Compile(defMarcoString)
+		macros := utils.RemoveRepeated(re.FindAllString(strings.ReplaceAll(macroString, " ", ""), -1))
+		if err := getMacroValue(macros, detectedMacroValue); err != nil {
+			return fmt.Errorf("get detect value failed: %v", err)
+		}
+		calcResult, err := utils.Calculate(convertString(macroString, detectedMacroValue))
+		if err != nil {
+			return fmt.Errorf("calculate err: %v", err)
+		}
+		range2Int = append(range2Int, int(calcResult))
+	}
+	param["range"] = range2Int
+	return nil
+}
+func convertString(macroString string, macroMap map[string]int) string {
+	retStr := strings.ReplaceAll(macroString, " ", "")
+	for name, value := range macroMap {
+		retStr = strings.ReplaceAll(retStr, name, fmt.Sprint(value))
+	}
+	return retStr
+}
+func getMacroValue(macros []string, detectedMacroValue map[string]int) error {
+	if len(macros) == 0 {
+		return nil
+	}
+	var macroMap = make(map[string]string)
+	detectFile := fmt.Sprintf("%v/detect/detect.json", config.KeenTune.Home)
+	bytes, err := ioutil.ReadFile(detectFile)
+	if err != nil {
+		return fmt.Errorf("read detect json file err:%v", err)
+	}
+	var macroNames []string
+	for _, macro := range macros {
+		if _, ok := detectedMacroValue[macro]; ok {
+			continue
+		}
+		name := strings.TrimSuffix(strings.TrimPrefix(macro, "#!"), "#")
+		lowerName := strings.ToLower(name)
+		var tempMap map[string]string
+		macroNames = append(macroNames, name)
+		err = json.Unmarshal(bytes, &tempMap)
+		if err != nil {
+			return fmt.Errorf("unmarshal detect json file err:%v", err)
+		}
+		index := 0
+		for key, value := range tempMap {
+			index++
+			if strings.ToLower(key) == lowerName {
+				macroMap[lowerName] = value
+				break
+			}
+			if index == len(tempMap) {
+				return fmt.Errorf("detect can't find matched macro :#!%v#", name)
+			}
+		}
+	}
+	if len(macroMap) == 0 {
+		return nil
+	}
+	return detect(macroMap, macroNames, detectedMacroValue)
+}
+func detect(macroMap map[string]string, macroNames []string, detectedMacroValue map[string]int) error {
+	requestMap := map[string]interface{}{
+		"data": macroMap,
+	}
+	url := fmt.Sprintf("%v:%v/detect", config.KeenTune.TargetIP[0], config.KeenTune.TargetPort)
+	resp, err := http.RemoteCall("POST", url, requestMap)
+	var respMap map[string]DetectResult
+	err = json.Unmarshal(resp, &respMap)
+	if err != nil {
+		return fmt.Errorf("unmarshal detect response err:%v", err)
+	}
+	for _, name := range macroNames {
+		result, ok := respMap[strings.ToLower(name)]
+		if !ok {
+			return fmt.Errorf("get response %v err:%v", name, err)
+		}
+		if !result.Success {
+			return fmt.Errorf("get response %v failed:%v", name, result.Message)
+		}
+		macro := fmt.Sprintf("#!%v#", name)
+		detectedMacroValue[macro] = result.Value
+	}
+	return nil
+}
+
