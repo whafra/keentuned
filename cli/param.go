d6056da371835db141b32b7664d972bf57c94870
lilinjie <lilinjie@uniontech.com>
2022-02-28 10:13:53 +0800

    Determine whether conf file already exists

--- cli/param.go
+++ cli/param.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	m "keentune/daemon/modules"
 )
 
 const (
@@ -157,7 +158,12 @@ func dumpCmd() *cobra.Command {
 				dump.Output = strings.TrimSuffix(dump.Output, ".conf") + ".conf"
 			}
 
-			RunDumpRemote(cmd.Context(), dump)
+			//Determine whether conf file already exists
+                        ProfilePath := m.GetProfileWorkPath(dump.Output)
+                        _, err := os.Stat(ProfilePath)
+                        if err == nil {
+                                RunDumpRemote(cmd.Context(), dump)
+                        }
 			return
 		},
 	}
