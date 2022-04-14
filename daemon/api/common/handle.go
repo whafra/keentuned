1b044c655a51c9d3453ecce7cfb5bcb23b508f50
liuwenchao <yuxiongkong159@gmail.com>
2022-04-14 10:10:36 +0000

    !110 feat: add cmd API for UI * feat: add cmd API for UI

--- daemon/api/common/handle.go
+++ daemon/api/common/handle.go
@@ -8,6 +8,7 @@ import (
 	"keentune/daemon/common/config"
 	"keentune/daemon/common/log"
 	"net/http"
+	"os/exec"
 	"strings"
 )
 
@@ -21,6 +22,7 @@ func registerRouter() {
 	http.HandleFunc("/apply_result", handler)
 	http.HandleFunc("/sensitize_result", handler)
 	http.HandleFunc("/status", status)
+	http.HandleFunc("/cmd", command)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -100,4 +102,110 @@ func status(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status": "alive"}`))
 	return
+}
+
+func command(w http.ResponseWriter, r *http.Request) {
+	var result = new(string)
+	var err error
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf("{\"suc\": false, \"msg\": \"%v\"}", err.Error())))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("{\"suc\": true, \"msg\": \"%s\"}", *result)))
+		return
+	}()
+
+	if strings.ToUpper(r.Method) != "POST" {
+		err = fmt.Errorf("request method \"%v\" is not supported", r.Method)
+		return
+	}
+
+	var cmd string
+	cmd, err = getCmd(r.Body)
+	if err != nil {
+		return
+	}
+
+	err = execCmd(cmd, result)
+	if err != nil {
+		return
+	}
+}
+
+func execCmd(inputCmd string, result *string) error {
+	cmd := exec.Command("/bin/bash", "-c", inputCmd)
+	// Create get command output pipeline
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("can not obtain stdout pipe for command:%s\n", err)
+	}
+
+	if err = cmd.Start(); err != nil {
+		return fmt.Errorf("command start err: %v", err)
+	}
+
+	bytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		return fmt.Errorf("ReadAll Stdout: %v", err.Error())
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return fmt.Errorf("wait: %v, output %s\n", err.Error(), bytes)
+	}	
+
+	if strings.Contains(string(bytes), "Y(yes)/N(no)") {
+		msg := strings.Split(string(bytes), "Y(yes)/N(no)")
+		if len(msg) != 2 {
+			return fmt.Errorf("get result %v", string(bytes))
+		}
+
+		*result = msg[1]
+		return nil
+	}
+
+	*result = getMsg(string(bytes), inputCmd)
+	return nil
+}
+
+func getMsg(origin, cmd string) string {
+	if strings.Contains(cmd, "-h") || strings.Contains(cmd, "sensitize list") {
+		return origin
+	}
+
+	pureMSg := strings.ReplaceAll(
+		strings.ReplaceAll(
+			strings.ReplaceAll(
+				origin, "\x1b[1;40;32m", ""),
+			"\x1b[0m", ""),
+		"\x1b[1;40;31m", "")
+
+	changeLinefeed := strings.ReplaceAll(pureMSg, "\n", "\\n")
+	changeTab := strings.ReplaceAll(changeLinefeed, "\t", " ")
+	return strings.TrimSuffix(changeTab, "\\n")
+}
+
+func getCmd(body io.ReadCloser) (string, error) {
+	bytes, err := ioutil.ReadAll(&io.LimitedReader{R: body, N: LimitBytes})
+	if err != nil {
+		return "", err
+	}
+
+	var reqInfo struct {
+		Cmd string `json:"cmd"`
+	}
+
+	err = json.Unmarshal(bytes, &reqInfo)
+	if err != nil {
+		return "", err
+	}
+
+	if strings.Contains(reqInfo.Cmd, "delete") {
+		return "echo y|" + reqInfo.Cmd, nil
+	}
+
+	return reqInfo.Cmd, nil
 }
