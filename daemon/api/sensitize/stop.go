b88b87791e423e2dab25ac3913e6ee15ba745c27
lilinjie <lilinjie@uniontech.com>
2022-04-14 07:20:24 +0000

    !107 Add check sensitize job stop * Add check sensitize job stop

--- daemon/api/sensitize/stop.go
+++ daemon/api/sensitize/stop.go
@@ -5,11 +5,18 @@ import (
 	m "keentune/daemon/modules"
 	"os"
 	"strings"
+	"fmt"
+        "keentune/daemon/common/log"
 )
 
 // Stop run sensitize stop service
 func (s *Service) Stop(request string, reply *string) error {
 	job := com.GetRunningTask()
+	if job == "" {
+                log.Errorf("", "No running job can stop.")
+                return fmt.Errorf("No running job can stop.")
+        }
+
 	if job != "" && (strings.Split(job, " ")[0] == com.JobCollection || strings.Split(job, " ")[0] == com.JobTraining) {
 		m.StopSig <- os.Interrupt
 	}
