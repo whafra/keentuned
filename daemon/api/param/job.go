d35754fe9c04265b700a8b8dedf5b5d41d120f37
lilinjie <lilinjie@uniontech.com>
2022-04-19 02:51:40 +0000

    !120 Modify param job command * Modify param job command

--- daemon/api/param/job.go
+++ daemon/api/param/job.go
@@ -1,9 +1,22 @@
 package param
 
 import (
-	"keentune/daemon/common/config"
 	"keentune/daemon/common/log"
+	"encoding/csv"
+	"os"
+	"io/ioutil"
+	"io"
+	"fmt"
 )
+
+type CSVRecord struct {
+	name string
+	algorithm string
+	iteration string
+	status string
+	starttime string
+	endtime string
+}
 
 // Jobs run param jobs service
 func (s *Service) Jobs(flag string, reply *string) error {
@@ -11,20 +24,52 @@ func (s *Service) Jobs(flag string, reply *string) error {
 		*reply = log.ClientLogMap[log.ParamJobs]
 		log.ClearCliLog(log.ParamJobs)
 	}()
-
-	jobHeader := "Tune Jobs"	
-
-	tuneJob, err := walkAndShow(config.GetTuningWorkPath("") + "/", "", true, jobHeader, "/generate/")
-	if err != nil {
-		log.Errorf(log.ParamJobs, "Walk path failed, err: %v", err)
-		return nil
-	}
-
-	if tuneJob == "" {
+	filepath := "/var/keentune/tuning_jobs.csv"
+	content, err := ioutil.ReadFile(filepath)
+	if string(content) == "" {
 		log.Infof(log.ParamJobs,"No job found")
 		return nil
 	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Errorf(log.ParamJobs, "Can not open the file, err: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 	
-	log.Infof(log.ParamJobs,"%v", tuneJob)
+	r := csv.NewReader(file)
+	var csvData []CSVRecord
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		var csvrecord CSVRecord
+		for index, value := range record {
+			switch index {
+			case 0:
+				csvrecord.name = value
+			case 1:
+				csvrecord.algorithm = value
+			case 2:
+				csvrecord.iteration = value
+			case 3:
+				csvrecord.status = value
+			case 4:
+				csvrecord.starttime = value
+			case 5:
+				csvrecord.endtime = value
+			}
+		}
+		csvData = append(csvData, csvrecord)
+	}
+	for _, v := range csvData {
+		var listInfo string
+		listInfo += fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v",v.name, v.algorithm, v.iteration, v.status, v. starttime, v.endtime) 
+	        log.Infof(log.ParamJobs,"%v", listInfo)
+
+	}
+
 	return nil
 }
