8afc6c02ddd3cdc2c57f99817bcf82716205c323
lilinjie <lilinjie@uniontech.com>
2022-04-20 10:02:27 +0000

    !125 Modify param stop command * Modify param stop command

--- daemon/api/param/stop.go
+++ daemon/api/param/stop.go
@@ -1,25 +1,74 @@
 package param
 
 import (
-	com "keentune/daemon/api/common"
 	m "keentune/daemon/modules"
 	"os"
-	"strings"
 	"fmt"
         "keentune/daemon/common/log"
+	"encoding/csv"
+	"io"
+	"bufio"
+	"strings"
 )
 
 // Stop run param stop service
 func (s *Service) Stop(request string, reply *string) error {
-	job := com.GetRunningTask()
-	if job == "" {
-                log.Errorf("", "No running job can stop.")
-                return fmt.Errorf("No running job can stop.")
-        }
+	fs, err := os.OpenFile("/var/keentune/tuning_jobs.csv", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Errorf("", "Can not open the file, err: %v\n",err)
+		return fmt.Errorf("Can not open the file.")
+	}
+	defer fs.Close()
+	r := csv.NewReader(fs)
+	var csvData []string
+	for {
+		row, err := r.Read()
+		if err == io.EOF {
+			break
+		}
 
-	if job != "" && (strings.Split(job, " ")[0] == com.JobTuning || strings.Split(job, " ")[0] == com.JobBenchmark) {
+		for index, value := range row {
+			switch index {
+			case 3:
+				csvData = append(csvData, value)
+			}
+		}
+	}
+
+	var status string
+	for _, v := range csvData {
+		status += v
+	}
+
+	if strings.Contains(status, "running") {
+		StrReplace("running", "abort")
 		m.StopSig <- os.Interrupt
+	} else {
+		log.Errorf("", "No running job can stop.")
+		return fmt.Errorf("No running job can stop.")
 	}
 
 	return nil
+}
+
+//StrReplace Modify csv file data
+func StrReplace(src string, dest string) {
+	out, _ := os.OpenFile("/var/keentune/tuning_jobs.csv", os.O_RDWR|os.O_CREATE, 0766)
+	defer out.Close()
+	in, _ := os.Open("/var/keentune/tuning_jobs.csv")
+	defer in.Close()
+
+	br := bufio.NewReader(in)
+	for {
+		line, _, err := br.ReadLine()
+		if err ==io.EOF {
+			break
+		}
+		newLine := strings.Replace(string(line), src, dest, -1)
+		_, err = out.WriteString(newLine + "\n")
+		if err != nil {
+			log.Errorf("", "replace err.")
+			os.Exit(1)
+		}
+	}
 }
