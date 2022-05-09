318fadc4fb6ce96caa379cb1f7fe2dd112efff95
shaojing <986461335@qq.com>
2022-05-09 12:56:19 +0000

    !202 add sensitive collect related test cases * test: add sensitive collect related test cases

--- test/CLI_reliability/main.py
+++ test/CLI_reliability/main.py
@@ -12,6 +12,7 @@ from CLI_reliability.test_profile_info import TestProfileInfo
 from CLI_reliability.test_profile_set import TestProfileSet
 from CLI_reliability.test_profile_delete import TestProfileDelete
 from CLI_reliability.test_profile_generate import TestProfileGenerate
+from CLI_reliability.test_sensitize_collect import TestSensitizeCollect
 from CLI_reliability.test_multi_scenes import TestMultiScenes
 from CLI_reliability.test_multi_target import TestMultiTarget
 
@@ -28,13 +29,16 @@ def RunReliabilityCase():
     profile_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestProfileDelete))
     profile_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestProfileGenerate))
 
+    sensitize_suite = unittest.TestSuite()
+    sensitize_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestSensitizeCollect))
+
     multi_scenes = unittest.TestSuite()
     multi_scenes.addTests(unittest.TestLoader().loadTestsFromTestCase(TestMultiScenes))
 
     multi_target = unittest.TestSuite()
     multi_target.addTests(unittest.TestLoader().loadTestsFromTestCase(TestMultiTarget))
 
-    suite = unittest.TestSuite([param_suite, profile_suite, multi_scenes, multi_target])
+    suite = unittest.TestSuite([param_suite, profile_suite, sensitize_suite, multi_scenes, multi_target])
     return suite
 
 
