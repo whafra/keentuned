746bed4c246d962ccab613e1fb065a4b8e1a5f33
liuwenchao <yuxiongkong159@gmail.com>
2022-05-20 02:55:50 +0000

    !241 feat: Support filtering query target group * feat: Support filtering query target group

--- cli/config.go
+++ cli/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"keentune/daemon/common/config"
+	"keentune/daemon/common/file"
 	"os"
 	"strings"
 )
@@ -39,21 +40,24 @@ func createConfigCmd() *cobra.Command {
 }
 
 func targetCmd() *cobra.Command {
+	var grep string
 	cmd := &cobra.Command{
 		Use:     "target",
 		Short:   "Show target group configuration",
 		Long:    "Show target group configuration",
 		Example: "keentune config target",
 		Run: func(cmd *cobra.Command, args []string) {
-			RunTargetRemote(cmd.Context())
+			RunTargetRemote(cmd.Context(), grep)
 			return
 		},
 	}
 
+	cmd.Flags().StringVar(&grep, "grep", "", "filter group with specified configuration")
+
 	return cmd
 }
 
-func RunTargetRemote(context context.Context) {
+func RunTargetRemote(context context.Context, grep string) {
 	err := config.InitTargetGroup()
 	if err != nil {
 		fmt.Printf("%v cat target config: %v", ColorString("red", "[ERROR]"), err)
@@ -61,11 +65,40 @@ func RunTargetRemote(context context.Context) {
 	}
 
 	var reply string
+	if grep == "" {
+		for _, group := range config.KeenTune.Target.Group {
+			reply += fmt.Sprintf("[target-group-%v]\n", group.GroupNo)
+			reply += fmt.Sprintf("TARGET_IP = %v\n", strings.Join(group.IPs, ","))
+		}
+
+		fmt.Printf("%v", reply)
+		return
+	}
+
+	filePath := config.GetProfileWorkPath("active.conf")
+	activeGroup := file.GetRecord(filePath, "name", grep, "group_info")
+	if len(activeGroup) == 0 {
+		fmt.Println("No matching group found")
+		return
+	}
+
+	actives := strings.Split(activeGroup, " ")
 	for _, group := range config.KeenTune.Target.Group {
-		reply += fmt.Sprintf("[target-group-%v]\n", group.GroupNo)
-		reply += fmt.Sprintf("TARGET_IP = %v\n", strings.Join(group.IPs, ","))
+		for _, info := range actives {
+			if strings.Contains(group.GroupName, strings.Trim(info, "group")) {
+				reply += fmt.Sprintf("[target-group-%v]\n", group.GroupNo)
+				reply += fmt.Sprintf("TARGET_IP = %v\n", strings.Join(group.IPs, ","))
+				break
+			}
+		}
+	}
+
+	if reply == "" {
+		fmt.Println("No matching group found")
+		return
 	}
 
 	fmt.Printf("%v", reply)
 	return
 }
+
