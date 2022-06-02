a732bafb6743c23b0b133e9961939b5d1f4956df
tianweiweiliang <875658254@qq.com>
2022-06-02 11:41:27 +0800

    fix: 拆分项目

--- web/src/pages/List/modules/ProfileList/service.ts
+++
@@ -1,9 +0,0 @@
-import { request } from 'umi'
-
-export const getRequestData = async (params: any) => {
-  return request('/cmd', { skipErrorHandler: true,
-      method: 'post',
-      data: params,
-      timeout: 5000,
-  })
-}
