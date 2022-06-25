30f188542d211c5c2ffdea11b82702f02ca3cf48
root <root@localhost.localdomain>
2022-06-26 02:46:53 +0800

    Modifying the Waiting Mode

--- test/KeenTune_UI/UI_base/test_experts_tuning_normal.py
+++ test/KeenTune_UI/UI_base/test_experts_tuning_normal.py
@@ -4,7 +4,8 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class TestKeenTune_UI_normal(unittest.TestCase):
     @classmethod
@@ -18,6 +19,7 @@ class TestKeenTune_UI_normal(unittest.TestCase):
             option.add_argument('--window-size=1920,1080')
             self.driver = webdriver.Chrome(options=option)
             self.driver.implicitly_wait(3)
+            self.wait = WebDriverWait(self.driver,20,0.5)
 
         else:
             if no_ui:
@@ -34,10 +36,19 @@ class TestKeenTune_UI_normal(unittest.TestCase):
 
     @classmethod
     def tearDownClass(self) -> None:
+        self.driver.get("http://39.102.55.119:8082/list/static-page")
+        for i in range(9):
+            first_text = self.driver.find_element(By.XPATH,'//tr[@data-row-key="1"]//td[1]//div[1]//span[1]').text
+            if first_text != 'cpu_high_load.conf':
+                self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="1"]//td[4]//div[1]//div[1]/span'))).click()
+                self.wait.until(EC.element_to_be_clickable((By.XPATH,'//div[@class="ant-popover-buttons"]/button[2]/span'))).click()
+            else:
+                break
         self.driver.quit()
 
+
     def test_copyfile(self):
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]/td[4]//div[2]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="1"]/td[4]//div[2]'))).click()
         self.driver.find_element(By.ID, "name").send_keys(Keys.CONTROL, "a")
         self.driver.find_element(By.ID, "name").send_keys(Keys.BACKSPACE)
         self.driver.find_element(By.ID, "name").send_keys("1")
@@ -47,17 +58,18 @@ class TestKeenTune_UI_normal(unittest.TestCase):
         assert "1.conf" in ele_copy.text
 
     def test_creatfile(self):
-        self.driver.find_element(By.XPATH,'//button[@class="ant-btn ant-btn-primary"]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//button[@class="ant-btn ant-btn-primary"]'))).click()
         self.driver.find_element(By.ID, "name").send_keys("11")
         self.driver.find_element(By.ID, "info").send_keys("[my.con]\ninnodb_file_per_table: 1")
         self.driver.find_element(By.XPATH,
                                  '//div[@class="ant-modal-mask"]/../div[2]/div[1]/div[2]/div[3]/div[1]/div[2]').click()
+        sleep(1)
         ele_creat = self.driver.find_element(By.XPATH, '//tr[@data-row-key="2"]//td[1]')
         sleep(1)
         assert "11.conf" in ele_creat.text
 
     def test_checkfile(self):
-        self.driver.find_element(By.XPATH,'//tr[@data-row-key="2"]/td[1]//span').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="2"]/td[1]//span'))).click()
         sleep(1)
         ele_checkfile = self.driver.find_element(By.XPATH,'//div[@class="CodeMirror-code"]')
         assert "[my.con]" in ele_checkfile.text
@@ -65,8 +77,7 @@ class TestKeenTune_UI_normal(unittest.TestCase):
                                  '//div[@class="ant-modal-mask"]/../div[2]/div[1]/div[2]/div[3]/button').click()
 
     def test_editor(self):
-        sleep(1)
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="2"]/td[4]//div[3]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="2"]/td[4]//div[3]/span'))).click()        
         self.driver.find_element(By.ID, "name").send_keys(Keys.CONTROL, "a")
         self.driver.find_element(By.ID, "name").send_keys(Keys.BACKSPACE)
         self.driver.find_element(By.ID, "name").send_keys("111")
@@ -77,55 +88,46 @@ class TestKeenTune_UI_normal(unittest.TestCase):
         assert "111.conf" in ele_editor.text
 
     def test_set_group(self):
-        sleep(2)
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]//td[4]//div[5]').click()
-        sleep(0.5)
-        self.driver.find_element(By.XPATH, '//input[@value="group1"]').click()
-        sleep(0.5)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="1"]//td[4]//div[5]'))).click()
+        self.driver.find_element(By.XPATH, '//label[@class="ant-radio-wrapper"]//span[2]').click()
         self.driver.find_element(By.XPATH, '//div[3]/div/div[2]/button/span').click()
-        sleep(3)
+        sleep(1)
         ele_set = self.driver.find_element(By.XPATH,'//tr[@data-row-key="1"]/td[3]')
         assert "[target-group-1]" in ele_set.text
 
     def test_restore(self):
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]/td[4]//div[4]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="1"]/td[4]//div[4]'))).click()
         sleep(1)
         ele_set = self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]/td[3]')
         assert "[target-group-1]\nTARGET_IP = localhost" not in ele_set.text
 
     def test_deletefile(self):
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]//td[4]//div[1]//div[1]').click()
-        sleep(0.5)
-        self.driver.find_element(By.XPATH, '//div[@class="ant-popover-buttons"]/button[2]').click()
-        sleep(0.5)
-        self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]//td[4]//div[1]//div[1]').click()
-        sleep(0.5)
-        self.driver.find_element(By.XPATH, '//div[@class="ant-popover-buttons"]/button[2]').click()
-        sleep(1)
+        for i in range(9):
+            first_text = self.driver.find_element(By.XPATH,'//tr[@data-row-key="1"]//td[1]//div[1]//span[1]').text
+            if first_text != 'cpu_high_load.conf':
+                self.wait.until(EC.element_to_be_clickable((By.XPATH,'//tr[@data-row-key="1"]//td[4]//div[1]//div[1]/span'))).click()
+                self.wait.until(EC.element_to_be_clickable((By.XPATH,'//div[@class="ant-popover-buttons"]/button[2]/span'))).click()
+            else:
+                break
         ele_copy = self.driver.find_element(By.XPATH, '//tr[@data-row-key="1"]//td[1]')
         assert "1.conf" not in ele_copy.text
 
     def test_language_switch(self):
-        self.driver.find_element(By.XPATH,
-                                 '//div[@class="ant-space ant-space-horizontal ant-space-align-center right___3L8KG"]/div/div/img').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                '//div[@class="ant-space ant-space-horizontal ant-space-align-center right___3L8KG"]/div/div/img'))).click()
         ele_language1 = self.driver.find_element(By.XPATH,
                                                  '//div[@class="ant-pro-table-list-toolbar-title"]')
         sleep(1)
         assert "List of Expert Knowledge Based Tuning Profiles" in ele_language1.text
-        ele_language2 = self.driver.find_element(By.XPATH, '//div[@class="ant-pro-table-list-toolbar-left"]//div')
-        sleep(1)
-        assert "List of Expert Knowledge Based Tuning Profiles"  in ele_language2.text
 
     def test_refresh(self):
-        self.driver.find_element(By.XPATH,
-                                 '//div[@class="ant-space ant-space-horizontal ant-space-align-center'
-                                 ' ant-pro-table-list-toolbar-setting-items"]/div[1]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//div[@class="ant-space ant-space-horizontal ant-space-align-center '
+                                                     'ant-pro-table-list-toolbar-setting-items"]//div[1]/div'))).click()
 
     def test_set_list(self):
-        self.driver.find_element(By.XPATH,
-                                 '//div[@class="ant-space ant-space-horizontal ant-space-align-center ant-pro-table-list-toolbar-setting-items"]//div[2]').click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, '//div[@class="ant-tree-list-holder-inner"]/div[1]//span[4]').click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                            '//div[@class="ant-space ant-space-horizontal ant-space-align-center ant-pro-table-list-toolbar-setting-items"]//div[2]'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,'//div[@class="ant-tree-list-holder-inner"]/div[1]//span[4]'))).click()
         sleep(1)
         ele = self.driver.find_element(By.XPATH, '//thead[@class="ant-table-thead"]')
         assert "Profile Name" not in ele.text
