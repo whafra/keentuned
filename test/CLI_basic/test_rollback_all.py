9fc6e572eb975531b9db1acce2bc1e97ec39c988
shaojing <986461335@qq.com>
2022-09-14 11:26:25 +0000

    !468 add test cases related to init and rollback * test: add test cases related to init and rollback

---
+++ test/CLI_basic/test_rollback_all.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import logging
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+from common import checkServerStatus
+from common import sysCommand
+
+logger = logging.getLogger(__name__)
+
+
+class TestRollbackAll(unittest.TestCase):
+    def setUp(self) -> None:
+        server_list = ["keentuned", "keentune-target"]
+        status = checkServerStatus(server_list)
+        self.assertEqual(status, 0)
+        logger.info('start to run test_rollback_all testcase')
+
+    def tearDown(self) -> None:
+        server_list = ["keentuned", "keentune-target"]
+        status = checkServerStatus(server_list)
+        self.assertEqual(status, 0)
+        logger.info('the test_rollback_all testcase finished')
+
+    def test_rollback_all_FUN(self):
+        cmd = 'keentune rollbackall'
+        self.status, self.out, _ = sysCommand(cmd)
+        self.assertEqual(self.status, 0)
+        self.assertTrue(self.out.__contains__('Rollback all successfully'))
+        self.status, self.out, _ = sysCommand(cmd)
+        self.assertEqual(self.status, 0)
+        self.assertTrue(self.out.__contains__('All Targets No Need to Rollback'))
