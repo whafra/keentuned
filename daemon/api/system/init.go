014bf5663a28c06c47dacc015bf1380996818816
liuwenchao <yuxiongkong159@gmail.com>
2022-09-20 09:52:44 +0000

    !478 feat: Modify init yaml structure * fix: Modify init yaml structure

--- daemon/api/system/init.go
+++ daemon/api/system/init.go
@@ -30,10 +30,8 @@ type ymlBench struct {
 }
 
 type keenTuneYML struct {
-	Bench  []ymlBench  `yaml:"bench"`
-	Brain  ymlBrain    `yaml:"brain"`
-	Hex    string      `yaml:"hex"`
-	Target []ymlTarget `yaml:"target"`
+	Bench []ymlBench
+	Brain ymlBrain
 }
 
 // Init ...
@@ -63,54 +61,68 @@ func initialize() (string, error) {
 	}
 
 	var ymlConf = &keenTuneYML{}
-	var warningDetail string
-	warningDetail = checkBenchAVL(ymlConf)
+	var warningDetail = new(string)
+	checkBenchAVL(ymlConf, warningDetail)
 
-	ymlConf.Brain.BrainIP = config.KeenTune.BrainIP
-	_, ymlConf.Brain.AlgoTune, ymlConf.Brain.AlgoSen, err = com.GetAVLDataAndAlgo()
+	checkBrainAVL(ymlConf, warningDetail)
+
+	targetGroup := checkTargetAVL(warningDetail)
+
+	bytes, err := yaml.Marshal(getYMLConf(ymlConf, targetGroup))
 	if err != nil {
-		warningDetail += fmt.Sprintf("\tbrain host %v unreachable\n", config.KeenTune.BrainIP)
-	}
-
-	targetResult := checkTargetAVL(ymlConf)
-	warningDetail += targetResult
-
-	ymlConf.Hex = config.KeenTuneConfMD5
-
-	bytes, err := yaml.Marshal(ymlConf)
-	if err != nil {
-		return warningDetail, err
+		return *warningDetail, err
 	}
 
 	err = ioutil.WriteFile(config.KeenTuneYMLFile, bytes, 0644)
-	return strings.TrimSuffix(warningDetail, "\n"), err
+	return strings.TrimSuffix(*warningDetail, "\n"), err
 }
 
-func checkTargetAVL(ymlConf *keenTuneYML) string {
-	var targetWarning string
+func checkBrainAVL(ymlConf *keenTuneYML, warningDetail *string) {
+	ymlConf.Brain.BrainIP = config.KeenTune.BrainIP
 	var err error
-	ymlConf.Target = make([]ymlTarget, len(config.KeenTune.IPMap))
-	for _, target := range config.KeenTune.Group {
+	_, ymlConf.Brain.AlgoTune, ymlConf.Brain.AlgoSen, err = com.GetAVLDataAndAlgo()
+	if err != nil {
+		*warningDetail += fmt.Sprintf("\tbrain host %v unreachable\n", config.KeenTune.BrainIP)
+	}
+}
+
+func getYMLConf(conf *keenTuneYML, group [][]ymlTarget) interface{} {
+	var ret = map[string]interface{}{}
+	ret["brain"] = conf.Brain
+	ret["bench-group-1"] = conf.Bench
+
+	for idx, target := range config.KeenTune.Group {
+		groupName := fmt.Sprintf("target-group-%v", target.GroupNo)
+		ret[groupName] = group[idx]
+	}
+
+	return ret
+}
+
+func checkTargetAVL(warningDetail *string) [][]ymlTarget {
+	var err error
+	targetGroup := make([][]ymlTarget, len(config.KeenTune.Group))
+	for groupIdx, target := range config.KeenTune.Group {
 		var tmpTarget ymlTarget
-		for _, ip := range target.IPs {
+		targetGroup[groupIdx] = make([]ymlTarget, len(target.IPs))
+		for ipIdx, ip := range target.IPs {
+			tmpTarget.Knobs = strings.Split(target.ParamConf, ",")
 			tmpTarget.Domain, err = com.GetAVLDomain(ip, target.Port)
 			if err != nil {
-				targetWarning += fmt.Sprintf("\ttarget host %v unreachable\n", ip)
+				*warningDetail += fmt.Sprintf("\ttarget host %v unreachable\n", ip)
+				targetGroup[groupIdx][ipIdx] = tmpTarget
 				continue
 			}
 
-			tmpTarget.Knobs = strings.Split(target.ParamConf, ",")
 			tmpTarget.IP = ip
-			idx := config.KeenTune.IPMap[ip] - 1
-			ymlConf.Target[idx] = tmpTarget
+			targetGroup[groupIdx][ipIdx] = tmpTarget
 		}
 	}
 
-	return targetWarning
+	return targetGroup
 }
 
-func checkBenchAVL(ymlConf *keenTuneYML) string {
-	var warningDetails string
+func checkBenchAVL(ymlConf *keenTuneYML, warningDetail *string) {
 	ymlConf.Bench = make([]ymlBench, len(config.KeenTune.BenchIPMap))
 	for _, bench := range config.KeenTune.BenchGroup {
 		var tmpBench ymlBench
@@ -119,11 +131,11 @@ func checkBenchAVL(ymlConf *keenTuneYML) string {
 		for _, ip := range bench.SrcIPs {
 			isBenchAVL, avlAgent, err := com.GetAVLAgentAddr(ip, bench.SrcPort, bench.DestIP)
 			if err != nil {
-				warningDetails += fmt.Sprintf("%v", err)
+				*warningDetail += fmt.Sprintf("%v", err)
 				if isBenchAVL {
 					tmpBench.IP = ip
 				}
-				
+
 				ymlConf.Bench[config.KeenTune.BenchIPMap[ip]-1] = tmpBench
 				continue
 			}
@@ -133,7 +145,5 @@ func checkBenchAVL(ymlConf *keenTuneYML) string {
 			ymlConf.Bench[config.KeenTune.BenchIPMap[ip]-1] = tmpBench
 		}
 	}
-
-	return warningDetails
 }
 
